03d3aa45956979defa5ded1233a3cd1cc9634ecc
Thuwapat <96274678+Thuwapat@users.noreply.github.com>
2025-02-15 21:38:11 +0700

    Bug fix

--- Get_Var.py
+++ Get_Var.py
@@ -80,9 +80,9 @@ def detect_hand(frame):
                 left_hand = (int((boxes[0][0] + boxes[0][2]) / 2), int((boxes[0][1] + boxes[0][3]) / 2))
                 right_hand = (int((boxes[1][0] + boxes[1][2]) / 2), int((boxes[1][1] + boxes[1][3]) / 2))
 
-                # ✅ Detect if hands are close
+                # Detect if hands are close
                 distance = np.sqrt((left_hand[0] - right_hand[0])**2 + (left_hand[1] - right_hand[1])**2)
-                hands_together = distance < 50 
+                hands_together = distance < 40 
 
     return left_hand, right_hand, handful, hand_center, hand_open, hands_together
 
